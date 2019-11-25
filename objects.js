79b08a9a594c0c07ebac8d0d6c68b318f7200b39
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-11-25 18:29:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,18 +1,7 @@
-var playlist = {
-  artist1: 'Slowdive',
-  artist2: 'My Bloody Valentine',
-  artist3:'Phil Ochs'
-};
-
-function updatePlaylist(thePlaylist, artistName, songTitle) {
-  return Object.assign({}, thePlaylist, { [artistName]: songTitle});
+var playlist={artistName:”song”};
+function updatePlaylist(playlist,artistName,songTitle){
+playlist[artistName]=songTitle
 }
-
-updatePlaylist(playlist, "artist4", "song4");
-
-function removeFromPlaylist(thePlaylist, artistName) {
-  delete thePlaylist[artistName];
-  return thePlaylist;
+function removeFromPlaylist(playlist, artistName){
+delete playlist[artistName];
 }
-
-removeFromPlaylist(playlist, "artist2");
